703da480538c5e180335e73be3e5f9d7dfdc80eb
Martin Valles <mvallesacc@gmail.com>
2023-08-22 13:10:00 -0300

    chore: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "rgb(var(--color-primary) / <alpha-value>)",
+      secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+      copy: "rgb(var(--color-copy) / <alpha-value>)",
+    },
     extend: {},
   },
   plugins: [],
