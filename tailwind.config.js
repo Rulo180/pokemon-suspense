85821dfaecd008790989b612d2cd82113f1f90d1
Martin Valles <mvallesacc@gmail.com>
2023-08-25 17:24:35 -0300

    fix(global): extend Tailwind color palette instead of replace it

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      primary: "rgb(var(--color-primary) / <alpha-value>)",
-      secondary: "rgb(var(--color-secondary) / <alpha-value>)",
-      copy: "rgb(var(--color-copy) / <alpha-value>)",
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        copy: "rgb(var(--color-copy) / <alpha-value>)",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
